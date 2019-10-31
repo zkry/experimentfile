e4fa51b0cf3d691695e82c02a874ecfbca0f8de2
Zac Romero <zacromero@posteo.net>
2019-10-31 11:47:58 +0100

    Add readme file along with logo

---
+++ README.md
@@ -0,0 +1,8 @@
+![Experimentfile Logo](/docs/logo.png)
+
+# Experimentfile
+
+An Experimentfile allows for the quick definition of various configurations and a specification
+of the traffic that they should get. Go code for working with this data is then generated anlong
+with the JSON configuration of the values. Such values can be read in during the run-time of a 
+Go program, allowing a complete decoupling of config/traffic from the Go code.
---
+++ docs/logo.rkt
@@ -0,0 +1,33 @@
+#lang racket
+(require pict
+         racket/draw)
+
+(define circle-size 10)
+
+(define (rgb r g b)
+  (make-object color% (exact-round r) (exact-round g) (exact-round b)))
+
+(define (rgb-list->color l)
+  (rgb (first l) (second l) (third l)))
+
+(define (random-color-offset val)
+  (let ([variation 100])
+    (max 0 (min 255 (+ val (- (random variation) (exact-round (/ variation 2))))))))
+
+(define (vari-conf-circles n r [rgb (list (random 256) (random 256) (random 256))])
+  (let* ([circles (for/list ((i (range n))) (rotate (vc-append
+                                                     (filled-ellipse circle-size circle-size #:color (rgb-list->color (map random-color-offset rgb)))
+                                                     (vline 10 (/ r 2))
+                                                     (blank 10 (/ r 2)))
+                                                    (* (/ (* 2 pi) n) i)))])
+    (apply cc-superimpose circles)))
+
+(define (logo) (cc-superimpose (filled-ellipse 45 45 #:draw-border? #f #:color "MediumTurquoise")
+                               (vari-conf-circles 15 60)
+                               (vari-conf-circles 7 30)))
+
+(define (save-pict the-pict name kind)
+  (define bm (pict->bitmap the-pict))
+  (send bm save-file name kind))
+
+(save-pict (logo) "logo.png" 'png)
