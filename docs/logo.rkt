0fe7ba25042b995fb16a1d6ba43834fe8a823535
Zac Romero <zacromero@posteo.net>
2019-10-31 13:07:29 +0100

    Update logo

--- docs/logo.rkt
+++ docs/logo.rkt
@@ -22,10 +22,13 @@
                                                     (* (/ (* 2 pi) n) i)))])
     (apply cc-superimpose circles)))
 
-(define (logo) (cc-superimpose (filled-ellipse 45 45 #:draw-border? #f #:color "MediumTurquoise")
-                               (vari-conf-circles 15 60)
-                               (vari-conf-circles 7 30)
-                               (blank 880 70)))
+(define (logo) (cc-superimpose
+                (vari-conf-circles 10 130)
+                (vari-conf-circles 60 90)
+                (filled-ellipse 45 45 #:draw-border? #f #:color "MediumTurquoise")
+                (vari-conf-circles 15 60)
+                (vari-conf-circles 7 30)
+                (blank 880 70)))
 
 (define (save-pict the-pict name kind)
   (define bm (pict->bitmap the-pict))
